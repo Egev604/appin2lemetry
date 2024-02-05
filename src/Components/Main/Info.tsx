042b6c1d8a0c4219809aa83f9b299f8b4b2e55fb
Dmitrii <egev604@mail.ru>
2024-02-05 15:10:40 +0300

    readyRouts

---
+++ src/Components/Main/Info.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Typography} from '@mui/material';
+
+const Info = () => {
+    return (
+        <Typography variant="h4" align="center" gutterBottom>
+            Welcome to Our Website!
+        </Typography>
+    );
+};
+
+export default Info;
