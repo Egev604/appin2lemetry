a64b62cd4d8472998bace476e61c2ec525bc6e73
Dmitrii <egev604@mail.ru>
2024-02-07 14:27:37 +0300

    readyUserMenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import {Box, Tabs, Tab, AppBar, Typography} from "@mui/material";
+import React, {useState} from 'react';
+import {Box, Tabs, Tab, AppBar, Typography, IconButton, Drawer, List, ListItem, ListItemText} from "@mui/material";
 import {Link} from 'react-router-dom';
 import NightModeToggle from './NightModeToggle';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Menu from "./Menu";
 //qwe
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
     return (
         <AppBar position="static">
             <Box sx={{width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
@@ -12,14 +19,17 @@ const Header = () => {
                         PC Load
                     </Typography>
                 </Link>
-                <Tabs aria-label="basic tabs example">
+                <Tabs aria-label="basic tabs example" style={{margin:10}} >
                     <Link to="/main" style={{textDecoration: 'none', color: 'black'}}>
                         <Tab label="Главная"/>
                     </Link>
                     <Link to="/info" style={{textDecoration: 'none', color: 'black'}}>
                         <Tab label="Информация"/>
                     </Link>
-                    <NightModeToggle/>
+                    <IconButton onClick={toggleMenu} size="large" edge="end" color="inherit"  >
+                        <AccountCircleIcon />
+                    </IconButton>
+                    <Menu toggleMenu={toggleMenu} isMenuOpen={isMenuOpen}/>
                 </Tabs>
             </Box>
         </AppBar>
