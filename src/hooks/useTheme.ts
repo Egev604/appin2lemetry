e3accdcc471bec1f44a6dab9ecdd26d10d75394e
Dmitrii <egev604@mail.ru>
2024-02-07 18:27:45 +0300

    ReadySavingTheme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -2,9 +2,14 @@ import React from 'react';
 import { createTheme } from '@mui/material';
 import getDesignTokens from '../theme/ThemeSettings';
 export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+const LOCAL_STORAGE_KEY = 'theme';
 export default function useTheme() {
-    const [mode, setMode] = React.useState<'light' | 'dark'>('light');
-
+    const [mode, setMode] = React.useState<'light' | 'dark'>(
+        () => (localStorage.getItem(LOCAL_STORAGE_KEY) as 'light' | 'dark') || 'light'
+    );
+    React.useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, mode);
+    }, [mode]);
     const colorMode = React.useMemo(
         () => ({
             toggleColorMode: () => {
