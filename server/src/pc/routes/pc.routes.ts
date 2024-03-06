8261980ada0ded3a952c7a50e86a539f8a3ab4b4
Dmitrii <egev604@mail.ru>
2024-03-06 13:54:29 +0300

    add middlewares

--- server/src/pc/routes/pc.routes.ts
+++ server/src/pc/routes/pc.routes.ts
@@ -3,12 +3,23 @@ import { body } from 'express-validator';
 
 import BodyValidationMiddleware from '../../common/middlewares/body.validator';
 import pcController from '../controller/pc.controller';
+import PcMiddlewares from '../middlewares/pc.middlewares';
 const bodyValidationMiddleware = new BodyValidationMiddleware();
 const pcRoutes = express.Router();
 pcRoutes.get('/', pcController.listPC);
-pcRoutes.delete('/:id', pcController.deletePC);
-pcRoutes.get('/:id', pcController.getPc);
-pcRoutes.put('/:id', pcController.updatePC);
+pcRoutes.delete('/:id', PcMiddlewares.validatePcExists, pcController.deletePC);
+pcRoutes.get('/:id', PcMiddlewares.validatePcExists, pcController.getPc);
+pcRoutes.put(
+    '/:id',
+    body('ComputerID').isNumeric(),
+    body('Model').isString(),
+    body('Description').isString(),
+    body('Condition').isString(),
+    body('components').optional().isObject(),
+    bodyValidationMiddleware.verifyBodyFieldsErrors,
+    PcMiddlewares.validatePcExists,
+    pcController.updatePC,
+);
 pcRoutes.post(
     '/',
     body('Model').isString(),
